8ba5300553cfb81952b4fd588bbfb47ca068d91d
Eugene <59817721+SKZGx@users.noreply.github.com>
2024-01-20 23:50:26 +0200

    modified:   brands.css 	deleted:    emoji.js 	modified:   en/index.html 	modified:   en/portfolio.html 	modified:   images.json 	modified:   index.html 	modified:   portfolio.css 	modified:   portfolio.html 	modified:   style.css 	new file:   tempimage/mytwitch.png 	new file:   tempimage/myyoutube.png

--- emoji.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-    // Parses the document body and    
-    // inserts <img> tags in place of Unicode Emojis    
-    twemoji.parse(document.body,
-        { folder: 'svg', ext: '.svg' } // This is to specify to Twemoji to use SVGs and not PNGs
-    );
-
-}
