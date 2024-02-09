08282ef882ac03af182fb87b858c643e5733e9d1
Eugene <59817721+SKZGx@users.noreply.github.com>
2024-02-09 11:45:47 +0200

    modified:   app.py 	modified:   images.json 	modified:   randomfooter.js 	modified:   templates/index.html

--- randomfooter.js
+++ randomfooter.js
@@ -4,14 +4,10 @@ document.addEventListener("DOMContentLoaded", function() {
         "url(https://i.imgur.com/h68yMHW.png)",
         "url(https://i.imgur.com/wGbvEX1.png)",
         "url(https://i.imgur.com/KGy5smy.png)",
-        "url(https://i.imgur.com/Zofg76B.png)",
         "url(https://i.imgur.com/eXkOnsg.png)",
         "url(https://i.imgur.com/VlyQ3pV.png)",
         "url(https://i.imgur.com/batTDxx.png)",
         "url(https://i.imgur.com/nKI5jUX.png)",
-        "url(https://i.imgur.com/Tq8wqfV.png)",
-        "url(https://i.imgur.com/nWGdCBX.png)"
-
 
         // Add more image URLs as needed
     ];
